1622c71241325000a47cf90b144ccdcf474d4e81
BANIICOFORI <baniicofori@gmail.com>
2022-09-10 17:45:40 +0000

    REduxs

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import Store from './Store/Store';
-import {createStore,applyMiddleware} from 'redux';
-import usersReducer from './Reducers/usersReducer';
-import thunk  from 'redux-thunk';
 import App from './App';
 
-const store =createStore (usersReducer,applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
